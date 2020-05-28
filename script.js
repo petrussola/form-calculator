d3eb5387ea1ba2bbaa3576b9a299e512425a6699
Pere <pere.solaclaver@gmail.com>
2020-05-29 00:30:02 +0100

    Add operations and ouput result

---
+++ script.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('form');
+const operand1 = document.getElementById('operand1');
+const operator = document.getElementById('operator');
+const operand2 = document.getElementById('operand2');
+const output = document.getElementById('form-output');
+
+// operate function
+function operate(operand1, operand2, operator) {
+	const num1 = parseFloat(operand1, 10);
+	const num2 = parseFloat(operand2, 10);
+	console.log(num1);
+	switch (operator) {
+		case 'add':
+			return num1 + num2;
+		case 'subtract':
+			return num1 - num2;
+		case 'multiply':
+			return num1 * num2;
+		case 'divide':
+			return num1 / num2;
+	}
+}
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const result = operate(operand1.value, operand2.value, operator.value);
+	const place = output.querySelector('h1');
+	place.innerText = result;
+});
